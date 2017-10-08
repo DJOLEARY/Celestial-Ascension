431304da5bc279a366fcaa99cfd32df120059b68
DarrenSweeney <darrensweeneydev@gmail.com>
2017-10-09 00:15:14 +0100

    Set up sfml for Debug - x86 build. Created copy command for dlls.

--- project3_ds_dj/main.cpp
+++ project3_ds_dj/main.cpp
@@ -1,8 +1,40 @@
-#include <iostream>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	std::cout << "Darren Sweeney commit" << std::endl;
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Project 3 - Darren & D.J");
+	// Load a sprite to display
+	sf::Texture texture;
+	if (!texture.loadFromFile("Assets/kitten.jpg"))
+		return EXIT_FAILURE;
+	sf::Sprite sprite(texture);
+	// Create a graphical text to display
+	sf::Font font;
+	if (!font.loadFromFile("Assets/arial.ttf"))
+		return EXIT_FAILURE;
+	sf::Text text("Hello SFML", font, 50);
 
-	return 0;
+	// Start the game loop
+	while (window.isOpen())
+	{
+		// Process events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// Close window : exit
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+		// Clear screen
+		window.clear();
+		// Draw the sprite
+		window.draw(sprite);
+		// Draw the string
+		window.draw(text);
+		// Update the window
+		window.display();
+	}
+	return EXIT_SUCCESS;
 }
