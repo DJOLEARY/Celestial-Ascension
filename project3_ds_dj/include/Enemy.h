fb9563f3ecc139bf2af18807b2f1bfaca8ac00f8
TheDjol <DJ@Computer.DJ>
2017-10-22 18:54:51 +0100

    Working on enemy class

--- project3_ds_dj/include/Enemy.h
+++ project3_ds_dj/include/Enemy.h
@@ -7,6 +7,7 @@
 class Enemy : public Entity
 {
 public:
+
     Enemy(sf::Vector2f *playerPos);    //  Enemy constructor.
     ~Enemy();   //  Enemy destructor.
 
@@ -14,9 +15,11 @@ public:
     virtual void Draw(sf::RenderWindow &renderWindow) override;
 private:
 
+
     void seekPlayer();
 
     sf::Vector2f *m_playerPos;
+
     float m_speed;
 };
 #endif // !ENEMY_H
