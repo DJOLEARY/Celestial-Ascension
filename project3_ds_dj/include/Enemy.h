907136c3183b64468e632553b2dfd2f0bf741a69
D.J. O'Leary <djoleary39@ymail.com>
2017-10-22 21:52:36 +0100

    Added enemy class

--- project3_ds_dj/include/Enemy.h
+++ project3_ds_dj/include/Enemy.h
@@ -7,7 +7,6 @@
 class Enemy : public Entity
 {
 public:
-
     Enemy(sf::Vector2f *playerPos);    //  Enemy constructor.
     ~Enemy();   //  Enemy destructor.
 
@@ -15,11 +14,8 @@ public:
     virtual void Draw(sf::RenderWindow &renderWindow) override;
 private:
 
-
     void seekPlayer();
-
     sf::Vector2f *m_playerPos;
-
     float m_speed;
 };
 #endif // !ENEMY_H
