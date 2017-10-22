b44cfd5584deb88d1c3a89a7909f1161ba964fef
TheDjol <DJ@Computer.DJ>
2017-10-22 19:04:14 +0100

    Added labels as gui element

---
+++ project3_ds_dj/src/GUI/Label.cpp
@@ -0,0 +1,105 @@
+#include "GUI/Label.h"
+
+/// <summary>
+/// Constructor function for the Label class
+/// </summary>
+/// <param name="text">Text to be displayed by the Label</param>
+/// <param name="parent">parent widget to the label</param>
+/// <param name="size">Font size of the Label</param>
+/// <param name="startPos">The start position of the transition</param>
+/// <param name="endPos">The end position of the transition</param>
+Label::Label(const std::string& text, unsigned int size, sf::Vector2f &startPos, sf::Vector2f &endPos)
+{
+	widgetStartPos = startPos;
+	widgetEndPos = endPos;
+	setPosition(startPos);
+
+	// @refactor(darren): Might want to create a resource manager
+	if (!m_gameFont.loadFromFile("Assets/Fonts/arial.ttf"))
+	{
+		std::cout << "ResourceManager:: gameFont failed to load!" << std::endl;
+	}
+	m_text.setFont(m_gameFont);
+	setText(text);
+	setTextColor(sf::Color::White);
+	m_text.setCharacterSize(size);
+	textRect = m_text.getGlobalBounds();
+
+	sf::Text fontCheck;
+	/*
+	We use this to make sure the heigh of the buttons is constant as the
+	bounding box IS affected by ascenders and descenders in strings so
+	the true text height is the maximum height possible
+	*/
+	fontCheck.setFont(*m_text.getFont());
+	fontCheck.setCharacterSize(size);
+	fontCheck.setString("ph|");
+	m_trueTextHeight = fontCheck.getGlobalBounds().height;
+	m_text.setOrigin(textRect.width / 2.0f,
+		m_trueTextHeight / 2.f);
+}
+
+/// <summary>
+/// Function simply sets the string for the text object
+/// </summary>
+/// <param name="text">string to be displayed</param>
+void Label::setText(const std::string& text)
+{
+	m_text.setString(text);
+}
+
+/// <summary>
+/// Function simply sets the position of the text object
+/// </summary>
+/// <param name="relativePosition"></param>
+void Label::setPosition(sf::Vector2f &position)
+{
+	m_text.setPosition(position);
+	widgetPos = position;
+}
+
+/// <summary>
+/// Function simply gets the character size used by the text object
+/// </summary>
+/// <returns></returns>
+unsigned int Label::getCharacterSize() const
+{
+	return m_text.getCharacterSize();
+}
+
+/// <summary>
+/// Set the color of the Text
+/// </summary>
+/// <param name="color">The desired text color</param>
+void Label::setTextColor(const sf::Color& color)
+{
+	m_text.setFillColor(color);
+}
+
+/// <summary>
+/// Draw function allows the window to draw the object directly
+/// </summary>
+/// <param name="target">Targets used for drawing</param>
+/// <param name="states">States used for drawing</param>
+void Label::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	target.draw(m_text, states);
+}
+
+/// <summary>
+/// Function simply returns the text object
+/// </summary>
+/// <returns>Text object</returns>
+sf::Text Label::getText()
+{
+	return m_text;
+}
+
+/// <summary>
+/// Function simply returns the maximum possible height for the bounding
+/// </summary>
+/// <returns>Max possible height of the text bounding rectangle</returns>
+float Label::getTextHeight()
+{
+	return m_trueTextHeight;
+}
--- project3_ds_dj/src/main.cpp
+++ project3_ds_dj/src/main.cpp
@@ -3,66 +3,63 @@
 #include "Player.h"
 #include "GUI/GUI.h"
 #include "GUI/Slider.h"
+#include "GUI\Label.h"
 
 int main()
 {
-	// Create the main window
-	sf::RenderWindow window(sf::VideoMode(1920, 1080), "Project 3 - Darren & D.J");
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(1920, 1080), "Project 3 - Darren & D.J");
 
-	EntityManager entityManager;
-	entityManager.Add(new Player());
+    EntityManager entityManager;
+    entityManager.Add(new Player());
 
-	// Create a graphical text to display
-	sf::Font font;
-	if (!font.loadFromFile("Assets/arial.ttf"))
-		return EXIT_FAILURE;
-	sf::Text text("Hello SFML", font, 50);
+    sf::Time timeSinceLastUpdate = sf::Time::Zero;
+    const sf::Time timePerFrame = sf::seconds(1.0f / 60.0f);
+    sf::Clock clock;
+    timeSinceLastUpdate = clock.restart();
 
-	sf::Time timeSinceLastUpdate = sf::Time::Zero;
-	const sf::Time timePerFrame = sf::seconds(1.0f / 60.0f);
-	sf::Clock clock;
-	timeSinceLastUpdate = clock.restart();
+    Gui guiSystem;
+    Slider *slider = new Slider(sf::Color(122, 12, 3, 255), sf::Color(22, 12, 3, 255),
+        sf::Color(2, 12, 3, 255), sf::Vector2f(100.0f, 100.0f));
+    Label *label = new Label("This is a lable", 24, sf::Vector2f(400, 40));
 
-	Gui guiSystem;
-	Slider *slider = new Slider(sf::Color(122, 12, 3, 255), sf::Color(22, 12, 3, 255),
-		sf::Color(2, 12, 3, 255), sf::Vector2f(100.0f, 100.0f));
-	slider->promoteFocus();
-	guiSystem.add(slider);
+    slider->promoteFocus();
+    guiSystem.add(slider);
+    guiSystem.add(label);
 
-	XboxController xboxController(CONTROLLER_TWO);
+    // @refactor(darren): I currently have two controller, makes controller a singleton?
+    XboxController xboxController(CONTROLLER_TWO);
 
-	// Start the game loop
-	while (window.isOpen())
-	{
-		// Process events
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			// Close window : exit
-			if (event.type == sf::Event::Closed)
-				window.close();
-		}
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window : exit
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
-		guiSystem.processInput(xboxController);
+        guiSystem.processInput(xboxController);
 
-		timeSinceLastUpdate += clock.restart();
-		if (timeSinceLastUpdate > timePerFrame)
-		{
-			entityManager.Update(timeSinceLastUpdate.asMilliseconds());
-			timeSinceLastUpdate = sf::Time::Zero;
-		}
+        timeSinceLastUpdate += clock.restart();
+        if (timeSinceLastUpdate > timePerFrame)
+        {
+            entityManager.Update(timeSinceLastUpdate.asMilliseconds());
+            timeSinceLastUpdate = sf::Time::Zero;
+        }
 
-		// Clear screen
-		window.clear();
+        // Clear screen
+        window.clear();
 
-		entityManager.Draw(window);
+        entityManager.Draw(window);
 
-		// Draw the string
-		//window.draw(text);
-		window.draw(guiSystem);
-		// Update the window
-		window.display();
-	}
+        window.draw(guiSystem);
+        // Update the window
+        window.display();
+    }
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
