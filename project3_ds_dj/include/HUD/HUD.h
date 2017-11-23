e9d30b6284259d37a147a4b3b4fa796902ceac02
DarrenSweeney <darrensweeneydev@gmail.com>
2017-11-23 14:32:47 +0000

    Alligned score and lives to hud when changed

--- project3_ds_dj/include/HUD/HUD.h
+++ project3_ds_dj/include/HUD/HUD.h
@@ -12,12 +12,18 @@ public:
 	void render(sf::RenderTexture &texture);
 	void setScore(uint16_t score);
 	void setWave(uint8_t wave);
+	void setLives(uint8_t lives);
 
 private:
 	sf::Texture m_leftHUDTex, m_rightHUDTex;
 	sf::Sprite m_leftHUDSprite, m_rightHUDSprite;
+	sf::Texture m_heartTexture;
+	sf::Sprite m_heartSprite;
 	sf::Font m_font;
 	sf::Text m_scoreText, m_waveNumText;
+	uint8_t m_lives;
+	static const int MAX_LIVES = 5;
+	sf::Vector2f m_heartPositions[MAX_LIVES];
 };
 
 #endif
